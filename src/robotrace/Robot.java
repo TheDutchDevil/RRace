9516780c84491ef8a48757416b7c9c98d25a6f37
Nathan Cassee <nathancassee@gmail.com>
2015-11-19 17:06:29 +0100

    First version of the stick figure

--- src/robotrace/Robot.java
+++ src/robotrace/Robot.java
@@ -8,6 +8,12 @@ import javax.media.opengl.glu.GLU;
 * Represents a Robot, to be implemented according to the Assignments.
 */
 class Robot {
+    /**
+     * Size of the robot in meters
+     */
+    private static final double SIZE = 2;
+    private static final double SKELETON_JOINT_RADIUS= (0.05*SIZE) / 2;
+    private static final double SKELETON_LIMB_RADIUS = SKELETON_JOINT_RADIUS /2;
     
     /** The position of the robot. */
     public Vector position = new Vector(0, 0, 0);
@@ -32,6 +38,163 @@ class Robot {
      * Draws this robot (as a {@code stickfigure} if specified).
      */
     public void draw(GL2 gl, GLU glu, GLUT glut, boolean stickFigure, float tAnim) {
-        // code goes here ...
+        gl.glPushMatrix();
+        
+        /**
+         * Translate 'to' the position of the robot 
+         */
+        gl.glTranslated(position.x, position.y, position.z);
+        
+        if(stickFigure) {
+           drawStickFigure(gl, glu, glut);
+        } else {
+            
+        }               
+        
+        gl.glPopMatrix();
     }
+
+    private void drawStickFigure(GL2 gl, GLU glu, GLUT glut) {
+        drawStickLeg(gl, glu, glut, true);
+        drawStickLeg(gl, glu, glut, false);
+        drawStickBody(gl, glu, glut);
+        drawStickArm(gl, glu, glut, true);
+        drawStickArm(gl, glu, glut, false);
+        drawStickHead(gl, glu, glut);
+    }
+
+    /**
+     * 
+     * @param leftSide Whether this stick leg is the left leg of the stick 
+     * figure. If this value is false it assumed it is the right leg. 
+     */
+    private void drawStickLeg(GL2 gl, GLU glu, GLUT glut, boolean leftLeg) {
+        gl.glPushMatrix();
+        
+        double footHeight = 0.05*SIZE;
+        
+        double translationOverXAxis = leftLeg ? -0.1*SIZE : 0.1*SIZE;
+        
+        gl.glTranslated(translationOverXAxis, 0, footHeight);
+        
+        glut.glutSolidSphere(SKELETON_JOINT_RADIUS, 32,32);
+        
+        glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .15*SIZE, 32, 32);
+        
+        gl.glTranslated(0, 0, .15*SIZE);
+        
+        glut.glutSolidSphere(SKELETON_JOINT_RADIUS, 32,32);
+        
+        glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .15*SIZE, 32, 32);
+        
+        gl.glTranslated(0, 0, .15*SIZE);
+        
+        glut.glutSolidSphere(SKELETON_JOINT_RADIUS, 32,32);
+        
+        gl.glPopMatrix();
+    }
+
+    private void drawStickBody(GL2 gl, GLU glu, GLUT glut) {
+       gl.glPushMatrix();
+       
+       gl.glTranslated(0,0, .35*SIZE);
+       
+       gl.glPushMatrix();
+       
+       gl.glTranslated(-.1*SIZE, 0, 0);
+       
+       gl.glRotated(90, 0, 1, 0);
+       
+       glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .2*SIZE, 32, 32);
+       
+       gl.glPopMatrix();
+       
+       glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .25*SIZE, 32, 32);
+       
+       gl.glTranslated(0, 0, .25*SIZE);
+       
+       gl.glPushMatrix();
+       
+       gl.glTranslated(-.1*SIZE, 0, 0);
+       
+       glut.glutSolidSphere(SKELETON_JOINT_RADIUS,32,32);
+       
+       gl.glRotated(90, 0, 1, 0);
+       
+       glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .2*SIZE, 32, 32);
+       
+       gl.glTranslated(0, 0, .2*SIZE);
+       
+       glut.glutSolidSphere(SKELETON_JOINT_RADIUS, 32, 32);
+       
+       gl.glPopMatrix();
+       
+       
+       gl.glPopMatrix();
+    }
+
+    private void drawStickArm(GL2 gl, GLU glu, GLUT glut, boolean leftArm) {
+        gl.glPushMatrix();
+        
+        double xAxisTranslation = leftArm ? -.1 * SIZE : .1*SIZE;
+        
+        gl.glTranslated(xAxisTranslation, 0, .6*SIZE);
+        
+        double upperArmRotation = leftArm ? -135 : 135;
+        
+        gl.glRotated(upperArmRotation, 0, 1, 0);
+        
+        glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .15*SIZE, 32, 32);
+        
+        gl.glTranslated(0, 0, .15*SIZE);
+        
+        glut.glutSolidSphere(SKELETON_JOINT_RADIUS, 32, 32);
+        
+        gl.glRotated(-90, 1, 0, 0);
+        
+        glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .15*SIZE, 32, 32);
+        
+        gl.glTranslated(0, 0, .15*SIZE);
+        
+        glut.glutSolidSphere(SKELETON_JOINT_RADIUS, 32, 32);
+        
+        gl.glPopMatrix();
+    }
+
+    private void drawStickHead(GL2 gl, GLU glu, GLUT glut) {
+        gl.glPushMatrix();
+        
+        gl.glTranslated(0,0, .6*SIZE);
+        
+        glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .1*SIZE, 32, 32);
+        
+        gl.glTranslated(0, 0, .1*SIZE);
+        
+        glut.glutSolidSphere(SKELETON_JOINT_RADIUS, 32, 32);
+        
+        glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .05*SIZE, 32, 32);
+        
+        gl.glTranslated(0, 0, .05*SIZE);
+        
+        gl.glPushMatrix();
+        
+        gl.glRotated(-90, 1, 0, 0);
+        
+        glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, 0.05*SIZE, 32, 32);
+        
+        gl.glTranslated(0, 0, .05*SIZE);
+        
+        glut.glutSolidSphere(SKELETON_JOINT_RADIUS, 32, 32);
+        
+        gl.glPopMatrix();
+        
+        glut.glutSolidCylinder(SKELETON_LIMB_RADIUS, .15*SIZE, 32, 32);
+        
+        gl.glTranslated(0, 0, .15*SIZE);
+        
+        glut.glutSolidSphere(SKELETON_JOINT_RADIUS, 32, 32);
+        
+        gl.glPopMatrix();
+    }
+    
 }
--- src/robotrace/RobotRace.java
+++ src/robotrace/RobotRace.java
@@ -4,172 +4,150 @@ import javax.media.opengl.GL;
 import static javax.media.opengl.GL2.*;
 
 /**
- * Handles all of the RobotRace graphics functionality,
- * which should be extended per the assignment.
- * 
- * OpenGL functionality:
- * - Basic commands are called via the gl object;
- * - Utility commands are called via the glu and
- *   glut objects;
- * 
- * GlobalState:
- * The gs object contains the GlobalState as described
- * in the assignment:
- * - The camera viewpoint angles, phi and theta, are
- *   changed interactively by holding the left mouse
- *   button and dragging;
- * - The camera view width, vWidth, is changed
- *   interactively by holding the right mouse button
- *   and dragging upwards or downwards;
- * - The center point can be moved up and down by
- *   pressing the 'q' and 'z' keys, forwards and
- *   backwards with the 'w' and 's' keys, and
- *   left and right with the 'a' and 'd' keys;
- * - Other settings are changed via the menus
- *   at the top of the screen.
- * 
- * Textures:
- * Place your "track.jpg", "brick.jpg", "head.jpg",
- * and "torso.jpg" files in the same folder as this
- * file. These will then be loaded as the texture
- * objects track, bricks, head, and torso respectively.
- * Be aware, these objects are already defined and
- * cannot be used for other purposes. The texture
- * objects can be used as follows:
- * 
- * gl.glColor3f(1f, 1f, 1f);
- * track.bind(gl);
- * gl.glBegin(GL_QUADS);
- * gl.glTexCoord2d(0, 0);
- * gl.glVertex3d(0, 0, 0);
- * gl.glTexCoord2d(1, 0);
- * gl.glVertex3d(1, 0, 0);
- * gl.glTexCoord2d(1, 1);
- * gl.glVertex3d(1, 1, 0);
- * gl.glTexCoord2d(0, 1);
- * gl.glVertex3d(0, 1, 0);
- * gl.glEnd(); 
- * 
- * Note that it is hard or impossible to texture
- * objects drawn with GLUT. Either define the
- * primitives of the object yourself (as seen
- * above) or add additional textured primitives
- * to the GLUT object.
+ * Handles all of the RobotRace graphics functionality, which should be extended
+ * per the assignment.
+ *
+ * OpenGL functionality: - Basic commands are called via the gl object; -
+ * Utility commands are called via the glu and glut objects;
+ *
+ * GlobalState: The gs object contains the GlobalState as described in the
+ * assignment: - The camera viewpoint angles, phi and theta, are changed
+ * interactively by holding the left mouse button and dragging; - The camera
+ * view width, vWidth, is changed interactively by holding the right mouse
+ * button and dragging upwards or downwards; - The center point can be moved up
+ * and down by pressing the 'q' and 'z' keys, forwards and backwards with the
+ * 'w' and 's' keys, and left and right with the 'a' and 'd' keys; - Other
+ * settings are changed via the menus at the top of the screen.
+ *
+ * Textures: Place your "track.jpg", "brick.jpg", "head.jpg", and "torso.jpg"
+ * files in the same folder as this file. These will then be loaded as the
+ * texture objects track, bricks, head, and torso respectively. Be aware, these
+ * objects are already defined and cannot be used for other purposes. The
+ * texture objects can be used as follows:
+ *
+ * gl.glColor3f(1f, 1f, 1f); track.bind(gl); gl.glBegin(GL_QUADS);
+ * gl.glTexCoord2d(0, 0); gl.glVertex3d(0, 0, 0); gl.glTexCoord2d(1, 0);
+ * gl.glVertex3d(1, 0, 0); gl.glTexCoord2d(1, 1); gl.glVertex3d(1, 1, 0);
+ * gl.glTexCoord2d(0, 1); gl.glVertex3d(0, 1, 0); gl.glEnd();
+ *
+ * Note that it is hard or impossible to texture objects drawn with GLUT. Either
+ * define the primitives of the object yourself (as seen above) or add
+ * additional textured primitives to the GLUT object.
  */
 public class RobotRace extends Base {
-    
-    /** Array of the four robots. */
-    private final Robot[] robots;
-    
-    /** Instance of the camera. */
-    private final Camera camera;
-    
-    /** Instance of the race track. */
-    private final RaceTrack[] raceTracks;
-    
-    /** Instance of the terrain. */
-    private final Terrain terrain;
-    
+
     /**
-     * Constructs this robot race by initializing robots,
-     * camera, track, and terrain.
+     * Array of the four robots.
+     */
+    private final Robot[] robots;
+
+    /**
+     * Instance of the camera.
+     */
+    private final Camera camera;
+
+    /**
+     * Instance of the race track.
+     */
+    private final RaceTrack[] raceTracks;
+
+    /**
+     * Instance of the terrain.
+     */
+    private final Terrain terrain;
+
+    /**
+     * Constructs this robot race by initializing robots, camera, track, and
+     * terrain.
      */
     public RobotRace() {
-        
+
         // Create a new array of four robots
         robots = new Robot[4];
-        
+
         // Initialize robot 0
         robots[0] = new Robot(Material.GOLD
-            /* add other parameters that characterize this robot */);
-        
+        /* add other parameters that characterize this robot */);
+
         // Initialize robot 1
         robots[1] = new Robot(Material.SILVER
-            /* add other parameters that characterize this robot */);
-        
+        /* add other parameters that characterize this robot */);
+
         // Initialize robot 2
         robots[2] = new Robot(Material.WOOD
-            /* add other parameters that characterize this robot */);
+        /* add other parameters that characterize this robot */);
 
         // Initialize robot 3
         robots[3] = new Robot(Material.ORANGE
-            /* add other parameters that characterize this robot */);
-        
+        /* add other parameters that characterize this robot */);
+
         // Initialize the camera
         camera = new Camera();
-        
+
         // Initialize the race tracks
         raceTracks = new RaceTrack[5];
-        
+
         // Test track
         raceTracks[0] = new RaceTrack();
-        
+
         // O-track
-        raceTracks[1] = new RaceTrack(new Vector[] {
-            /* add control points like:
+        raceTracks[1] = new RaceTrack(new Vector[]{ /* add control points like:
             new Vector(10, 0, 1), new Vector(10, 5, 1), new Vector(5, 10, 1),
             new Vector(..., ..., ...), ...
-            */
-        });
-        
+         */});
+
         // L-track
-        raceTracks[2] = new RaceTrack(new Vector[] { 
-            /* add control points */
-        });
-        
+        raceTracks[2] = new RaceTrack(new Vector[]{ /* add control points */});
+
         // C-track
-        raceTracks[3] = new RaceTrack(new Vector[] { 
-            /* add control points */
-        });
-        
+        raceTracks[3] = new RaceTrack(new Vector[]{ /* add control points */});
+
         // Custom track
-        raceTracks[4] = new RaceTrack(new Vector[] { 
-           /* add control points */
-        });
-        
+        raceTracks[4] = new RaceTrack(new Vector[]{ /* add control points */});
+
         // Initialize the terrain
         terrain = new Terrain();
     }
-    
+
     /**
-     * Called upon the start of the application.
-     * Primarily used to configure OpenGL.
+     * Called upon the start of the application. Primarily used to configure
+     * OpenGL.
      */
     @Override
     public void initialize() {
-		
+
         // Enable blending.
         gl.glEnable(GL_BLEND);
         gl.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-                
+
         // Enable depth testing.
         gl.glEnable(GL_DEPTH_TEST);
         gl.glDepthFunc(GL_LESS);
-		
-	    // Normalize normals.
+
+        // Normalize normals.
         gl.glEnable(GL_NORMALIZE);
-        
+
         // Enable textures. 
         gl.glEnable(GL_TEXTURE_2D);
         gl.glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
         gl.glBindTexture(GL_TEXTURE_2D, 0);
-		
-	    // Try to load four textures, add more if you like.
+
+        // Try to load four textures, add more if you like.
         track = loadTexture("track.jpg");
         brick = loadTexture("brick.jpg");
         head = loadTexture("head.jpg");
         torso = loadTexture("torso.jpg");
-        
+
         /**
-         * Initials values for theta, phi and vDist so that the eye is placed 
-         * to look directly into the scene. 
+         * Initials values for theta, phi and vDist so that the eye is placed to
+         * look directly into the scene.
          */
-        gs.theta = (float)Math.PI / 4f;
-        gs.phi = (float)Math.PI / 3f;
-        
+        gs.theta = (float) Math.PI / 4f;
+        gs.phi = (float) Math.PI / 3f;
+
         gs.vDist = 7;
     }
-    
+
     /**
      * Configures the viewing transform.
      */
@@ -177,87 +155,89 @@ public class RobotRace extends Base {
     public void setView() {
         // Select part of window.
         gl.glViewport(0, 0, gs.w, gs.h);
-        
+
         // Set projection matrix.
         gl.glMatrixMode(GL_PROJECTION);
         gl.glLoadIdentity();
 
         /**
-        * Specifies a viewing frustrum based on a viewing angle in the y 
-        * direction, aspect ratio and a near and far clipping plane. Values for
-        * the near and far clipping plane (Last two parameters) are based on the
-        * values mentioned in the assignment document. 
-        */
-        glu.gluPerspective(40, (float)gs.w / (float)gs.h, 0.1 * gs.vDist, 10*gs.vDist);
-        
+         * Specifies a viewing frustrum based on a viewing angle in the y
+         * direction, aspect ratio and a near and far clipping plane. Values for
+         * the near and far clipping plane (Last two parameters) are based on
+         * the values mentioned in the assignment document.
+         */
+        glu.gluPerspective(40, (float) gs.w / (float) gs.h, 0.1 * gs.vDist, 10 * gs.vDist);
+
         // Set camera.
         gl.glMatrixMode(GL_MODELVIEW);
         gl.glLoadIdentity();
-               
+
         // Update the view according to the camera mode and robot of interest.
         // For camera modes 1 to 4, determine which robot to focus on.
         camera.update(gs, robots[0]);
-        glu.gluLookAt(camera.eye.x(),    camera.eye.y(),    camera.eye.z(),
-                      camera.center.x(), camera.center.y(), camera.center.z(),
-                      camera.up.x(),     camera.up.y(),     camera.up.z());
+        glu.gluLookAt(camera.eye.x(), camera.eye.y(), camera.eye.z(),
+                camera.center.x(), camera.center.y(), camera.center.z(),
+                camera.up.x(), camera.up.y(), camera.up.z());
     }
-    
+
     /**
      * Draws the entire scene.
      */
     @Override
     public void drawScene() {
+        
+        gs.showStick = true;
         // Background color.
         gl.glClearColor(1f, 1f, 1f, 0f);
-        
+
         // Clear background.
         gl.glClear(GL_COLOR_BUFFER_BIT);
-        
+
         // Clear depth buffer.
         gl.glClear(GL_DEPTH_BUFFER_BIT);
-        
+
         // Set color to black.
         gl.glColor3f(0f, 0f, 0f);
-        
+
         gl.glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        
+
         // Draw the axis frame.
         if (gs.showAxes) {
             drawAxisFrame();
         }
-               
+
         // Get the position and direction of the first robot.
         robots[0].position = raceTracks[gs.trackNr].getLanePoint(0, 0);
         robots[0].direction = raceTracks[gs.trackNr].getLaneTangent(0, 0);
-        
+
         // Draw the first robot.
-        robots[0].draw(gl, glu, glut, false, gs.tAnim);
-        
+        robots[0].draw(gl, glu, glut, gs.showStick, gs.tAnim);
+
         // Draw the race track.
         raceTracks[gs.trackNr].draw(gl, glu, glut);
-        
+
         // Draw the terrain.
         terrain.draw(gl, glu, glut);
-        
+
         // Unit box around origin.
         glut.glutWireCube(1f);
 
         // Move in x-direction.
         gl.glTranslatef(2f, 0f, 0f);
-        
+
         // Rotate 30 degrees, around z-axis.
         gl.glRotatef(30f, 0f, 0f, 1f);
-        
+
         // Scale in z-direction.
         gl.glScalef(1f, 1f, 2f);
 
         // Translated, rotated, scaled box.
         glut.glutWireCube(1f);
     }
-    
+
     /**
-     * Draws the x-axis (red), y-axis (green), z-axis (blue),
-     * and origin (yellow).
+     * Draws the x-axis (red), y-axis (green), z-axis (blue), and origin
+     * (yellow).
      */
     public void drawAxisFrame() {
         // set color to yellow
@@ -266,26 +246,31 @@ public class RobotRace extends Base {
         // red arrow on x as
         // set color to red
         gl.glPushMatrix();
-        gl.glColor3f(1f, 0f, 0f);
-        // rotat to x-as
-        gl.glRotatef(90f, 0f, 1f, 0f);
-        gl.glTranslatef(0f, 0f, 0.25f);
-        gl.glScalef(0.1f, 0.1f, 1f);
-        glut.glutSolidCube(0.5f);
-        gl.glTranslatef(0f, 0f, 0.25f);
-        glut.glutSolidCone(0.6, 0.5, 30, 30);
+        {
+            gl.glColor3f(1f, 0f, 0f);
+            // rotat to x-as
+            gl.glRotatef(90f, 0f, 1f, 0f);
+            gl.glTranslatef(0f, 0f, 0.25f);
+            gl.glScalef(0.1f, 0.1f, 1f);
+            glut.glutSolidCube(0.5f);
+            gl.glTranslatef(0f, 0f, 0.25f);
+            glut.glutSolidCone(0.6, 0.5, 30, 30);
+        }
         gl.glPopMatrix();
+
         // green arrow on the y-as
         // set color to green
         gl.glColor3f(0f, 1f, 0f);
         // rotat to y-as
         gl.glPushMatrix();
-        gl.glRotatef(-90f, 1f, 0f, 0f);
-        gl.glTranslatef(0f, 0f, 0.25f);
-        gl.glScalef(0.1f, 0.1f, 1f);
-        glut.glutSolidCube(0.5f);
-        gl.glTranslatef(0f, 0f, 0.25f);
-        glut.glutSolidCone(0.6, 0.5, 30, 30);
+        {
+            gl.glRotatef(-90f, 1f, 0f, 0f);
+            gl.glTranslatef(0f, 0f, 0.25f);
+            gl.glScalef(0.1f, 0.1f, 1f);
+            glut.glutSolidCube(0.5f);
+            gl.glTranslatef(0f, 0f, 0.25f);
+            glut.glutSolidCone(0.6, 0.5, 30, 30);
+        }
         gl.glPopMatrix();
         // gl.glLoadIdentity();
         // bluw arrow on the z-as
@@ -293,22 +278,24 @@ public class RobotRace extends Base {
         gl.glColor3f(0f, 0f, 1f);
         // rotat to y-as
         gl.glPushMatrix();
-        //gl.glRotatef(90f, 1f, 0f, 0f);
-        gl.glTranslatef(0f, 0f, 0.25f);
-        gl.glScalef(0.1f, 0.1f, 1f);
-        glut.glutSolidCube(0.5f);
-        gl.glTranslatef(0f, 0f, 0.25f);
-        glut.glutSolidCone(0.6, 0.5, 30, 30);
+        {
+            //gl.glRotatef(90f, 1f, 0f, 0f);
+            gl.glTranslatef(0f, 0f, 0.25f);
+            gl.glScalef(0.1f, 0.1f, 1f);
+            glut.glutSolidCube(0.5f);
+            gl.glTranslatef(0f, 0f, 0.25f);
+            glut.glutSolidCone(0.6, 0.5, 30, 30);
+        }
         gl.glPopMatrix();
-        
+
     }
- 
+
     /**
-     * Main program execution body, delegates to an instance of
-     * the RobotRace implementation.
+     * Main program execution body, delegates to an instance of the RobotRace
+     * implementation.
      */
     public static void main(String args[]) {
         RobotRace robotRace = new RobotRace();
         robotRace.run();
-    } 
+    }
 }
